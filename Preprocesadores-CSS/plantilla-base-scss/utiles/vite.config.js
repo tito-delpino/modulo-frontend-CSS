5c19faf11f70b2b604adec4f33cb3765170801f2
Alvaro Del Pino <alvarodelpino88@gmail.com>
2025-12-19 06:44:01 +0100

    nuevo token

--- Preprocesadores-CSS/plantilla-base-scss/utiles/vite.config.js
+++ Preprocesadores-CSS/plantilla-base-scss/utiles/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     host: true, // Añadiendo host:true para poder acceder desde cualquier dispositivo de tu red, como por ejemplo para probar el responsive desde tu teléfono o tu tablet conectado a la misma Wifi/red. By @Eleazar
     watch: {
-      usePolling: true, // Recomendado para WSL, permite detectar cambios en archivos
+      usePolling: true, // Recomendado para WSL, permite detectar cambios en archivo
     },
   },
 });
