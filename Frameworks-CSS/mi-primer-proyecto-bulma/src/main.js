cfa3db0035fa869b7bd3d44805efd94f6f2b3c58
Alvaro Del Pino <alvarodelpino88@gmail.com>
2026-01-21 06:42:42 +0100

    prueba con bulma lista

--- Frameworks-CSS/mi-primer-proyecto-bulma/src/main.js
+++ Frameworks-CSS/mi-primer-proyecto-bulma/src/main.js
@@ -1,2 +1,16 @@
 import "bulma/css/bulma.min.css";
 import "./style.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const burgers = document.querySelectorAll(".navbar-burger");
+
+  burgers.forEach((burger) => {
+    burger.addEventListener("click", () => {
+      const targetId = burger.dataset.target;
+      const target = document.getElementById(targetId);
+
+      burger.classList.toggle("is-active");
+      target.classList.toggle("is-active");
+    });
+  });
+});
